9645518cfba388ddf08bb881a8ddc9684f681394
Alexander Kiefer <alexanderkiefer@MacBook-Pro.local>
2022-09-07 21:43:15 +0200

    Added github to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Github } from 'styled-icons/evaicons-solid/';
 
 const Wrapper = styled.div`
     background: ${({ theme }) => theme.colors.darkGrey};
     padding: 10px;
     display: flex;
     justify-content: flex-end;
+    align-items: center;
 `;
 
 const Link = styled.a`
     color: ${({ theme }) => theme.colors.fg};
     text-decoration: none;
-    margin-left: 10px;
+    margin-left: 20px;
     font-size: 0.8em;
 `;
 
@@ -23,6 +25,9 @@ const Index = () => {
             </Link>
             <Link href="https://kiefer.media/kontakt" target="_blank">
                 Impressum
+            </Link>
+            <Link href="https://github.com/schlaxel/rfuticket" target="_blank">
+                <Github size={20} />
             </Link>
         </Wrapper>
     );
